514e773cd2c7498523d4ff3e482c875c91a2c4e6
j3ffh95 <j3ffh95@gmail.com>
2021-08-03 22:02:57 -0400

    Added intructions

--- week2-hw3-linear_search/script.js
+++ week2-hw3-linear_search/script.js
@@ -1,1 +1,4 @@
-console.log("hello");
+// - Create an array of at least 10 values.
+// - Take an input from the user to look for in the array
+// - Find and display the total number of occurrences of the user input value in the array
+// - Also, display the positions in which they occur.      HINT: Create another array for keeping track of positions

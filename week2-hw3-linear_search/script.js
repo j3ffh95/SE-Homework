cde7d6f698d682596eb157c947d77c7cc9cffad5
j3ffh95 <j3ffh95@gmail.com>
2021-08-03 22:03:54 -0400

    Created array

--- week2-hw3-linear_search/script.js
+++ week2-hw3-linear_search/script.js
@@ -2,3 +2,5 @@
 // - Take an input from the user to look for in the array
 // - Find and display the total number of occurrences of the user input value in the array
 // - Also, display the positions in which they occur.      HINT: Create another array for keeping track of positions
+
+let array = [1, 3, -10, 65, 12, -34, 2, 15, -43, 10];

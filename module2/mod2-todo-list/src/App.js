872e0f7e64d5b9c4ef42955cedfe7aee683c448f
Jefferson Huisa <j3ffh95@gmail.com>
2021-09-14 10:29:08 -0400

    Added useeffect

--- module2/mod2-todo-list/src/App.js
+++ module2/mod2-todo-list/src/App.js
@@ -2,7 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  const [todoItems, setTodoItems] = useState([]);
+
+  // only run once the first time this component is rendered
+  useEffect(() => {
+    if (localStorage.getItem("todoData")) {
+      setTodoItems(JSON.parse(localStorage.getItem("todoData")));
+    }
+  }, []);
+
+  // run every time our pet state changes
+  useEffect(() => {
+    localStorage.setItem("examplePetData", JSON.stringify(todoItems));
+  }, [todoItems]);
+
+  return (
+    <div className='App'>
+      <h1>Todo App</h1>
+    </div>
+  );
 }
 
 export default App;

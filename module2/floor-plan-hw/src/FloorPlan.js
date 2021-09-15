2f182a56e3976e2938c62dcdeee7fa267f487823
Jefferson Huisa <j3ffh95@gmail.com>
2021-09-15 10:10:06 -0400

    Added the floorplan the components necessary

---
+++ module2/floor-plan-hw/src/FloorPlan.js
@@ -0,0 +1,17 @@
+import Kitchen from "./Kitchen";
+import LivingRoom from "./LivingRoom";
+import Bedroom from "./Bedroom";
+import Bath from "./Bath";
+
+function FloorPlan() {
+  return (
+    <>
+      <Kitchen />
+      <LivingRoom />
+      <Bedroom />
+      <Bath />
+    </>
+  );
+}
+
+export default FloorPlan;
